992cab50db0c4fce559e55996e8ec1a2284982f7
Ugnius <ugniusnaujokas@gmail.com>
2020-01-04 13:41:38 +0200

    Pridetas zemes turis;

--- uzduotis1/uzduotis1/Program.cs
+++ uzduotis1/uzduotis1/Program.cs
@@ -12,8 +12,7 @@ namespace uzduotis1
         {
             int ZemesSpindulys = 6371;
             double ZemesPlotas = 4 * Math.PI * Math.Pow(ZemesSpindulys, 2);
-            
-            
+            double ZemesTuris = 4 / 3 * Math.PI * Math.Pow(ZemesSpindulys, 3);            
         
         }
     }
